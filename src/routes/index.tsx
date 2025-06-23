224305751d08ee119a25447ffeab4f40bbdf2746
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-06-22 21:54:37 -0500

    style

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -81,7 +81,7 @@ function App() {
             </button>
             <button
               onClick={handleClickReset}
-              className="px-6 py-2 bg-red-900 text-white rounded-lg hover:bg-red-800 transition-colors"
+              className="px-6 py-2 bg-red-700 text-white rounded-lg hover:bg-red-800 transition-colors"
             >
               Reset
             </button>
