cacc48a2a3d6afcd41234f11d061d665d9855aeb
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-06-22 22:15:35 -0500

    forceSafariHLS

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -38,6 +38,7 @@ export const VideoPlayer = ({
           config={{
             file: {
               forceHLS: true,
+              forceSafariHLS: true,
               hlsOptions: {
                 // https://github.com/video-dev/hls.js/blob/master/docs/API.md#fine-tuning
                 maxBufferLength: 30,
